42663d3b65f65fd481d36fa8463e48dccb7befd6
GameDevelipers <gamedevelipers@outlook.com>
2024-11-29 17:46:16 +0000

    Update sign-in.js

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -4,7 +4,7 @@ export default function SignInPage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <div className="p-6 bg-white rounded-lg shadow-lg">
-        <h2 className="text-2xl font-bold mb-4 text-center">Sign In</h2>
+        <h2 className="text-2xl font-bold mb-4 text-center"></h2>
         <SignIn path="/sign-in" routing="path" />
       </div>
     </div>
