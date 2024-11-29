f5b2c08774be6d408cbd09034a138869f890958d
GameDevelipers <gamedevelipers@outlook.com>
2024-11-29 17:34:06 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,12 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div style={{ textAlign: "center", padding: "2rem" }}>
-      <h1>Welcome to ArchieMProductions</h1>
+      <h1>GameDevelipers Secure Site</h1>
       <SignedIn>
-        <p>You are signed in! 🎉</p>
+        <p>Signed in</p>
       </SignedIn>
       <SignedOut>
-        <p>Please sign in to access exclusive content.</p>
+        <p>Please sign in to access.</p>
         <Link href="/sign-in">
           <button>Sign In</button>
         </Link>
