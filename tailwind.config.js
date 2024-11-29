9f63f244e179183ffc19da5275b3c12db37c6993
GameDevelipers <gamedevelipers@outlook.com>
2024-11-29 17:49:41 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  theme: {
+    extend: {
+      animation: {
+        'slow-pulse': 'pulse 5s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+    },
+  },
+};
   theme: {
     extend: {},
   },
